033219724065a604c5b4014a0767d2e60f9d96da
Dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2019-12-14 18:06:49 +0200

    Added shop page and collection preview

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collection: SHOP_DATA
+        }
+    }
+
+    render() {
+        const collection = this.state.collection;
+        return (<div className="shop-page">
+            {
+                collection.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps}/>
+                ))
+            }
+        </div>)
+    }
+}
+
+export default ShopPage;
