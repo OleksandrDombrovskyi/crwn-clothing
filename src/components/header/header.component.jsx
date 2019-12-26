ee19e14cb691e813e4db586149c91e15e4978d98
Dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2019-12-26 20:53:47 +0200

    created user selector, introduced createStructuredSelector, refactored components regarding new concept

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,9 @@ import {auth} from "../../firebase/firebase.utils";
 import {connect} from 'react-redux';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import {createStructuredSelector} from 'reselect';
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/user/user.selector";
 
 const Header = ({currentUser, isCartDropdownHidden}) => (
     <div className="header">
@@ -30,9 +33,9 @@ const Header = ({currentUser, isCartDropdownHidden}) => (
     </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    isCartDropdownHidden: hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    isCartDropdownHidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
