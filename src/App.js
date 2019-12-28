8aa563fb0dabd47a490a3622d0f62585b63d1729
Dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2019-12-28 16:30:22 +0200

    added checkout page, added checkout item component, updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {Redirect, Route, Switch} from 'react-router-dom';
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import {connect} from 'react-redux';
 import {setCurrentUser} from "./redux/user/user.actions";
 import {selectCurrentUser} from "./redux/user/user.selector";
 import {createStructuredSelector} from 'reselect';
 
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import HomePage from "./pages/homepage/homepage.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 class App extends React.Component {
 
@@ -45,7 +46,8 @@ class App extends React.Component {
                 <Header/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
-                    <Route exect path='/shop' component={ShopPage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route exect path='/checkout' component={CheckoutPage}/>
                     <Route exect path='/signin' render={() =>
                         this.props.currentUser
                             ? (<Redirect to='/'/>)
