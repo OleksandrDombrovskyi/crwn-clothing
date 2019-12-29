c6209cb6a9718b89cd6eff7af7985df55bb93b05
Dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2019-12-29 19:01:07 +0200

    added styled component, converted some of components styling to *.jsx files instead of scss

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import './collection-overview.styles.scss';
 
 import {connect} from 'react-redux';
 import {createStructuredSelector} from "reselect";
 import CollectionPreview from "../preview-collection/collection-preview.component";
 import {selectCollectionForPreview} from "../../redux/shop/shop.selectors";
+import {CollectionOverviewContainer} from "./collection-overview.styles";
 
 const CollectionsOverview = ({collections}) => (
-    <div className='collections-overview'>
+    <CollectionOverviewContainer>
         {
             collections.map(({id, ...otherCollectionProps}) => (
                 <CollectionPreview key={id} {...otherCollectionProps}/>
             ))
         }
-    </div>
+    </CollectionOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import './header.style.scss'
-import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../../assets/crown.svg";
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from 'react-redux';
@@ -9,28 +7,29 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import {createStructuredSelector} from 'reselect';
 import {selectCartHidden} from "../../redux/cart/cart.selectors";
 import {selectCurrentUser} from "../../redux/user/user.selector";
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from "./header.styles";
 
 const Header = ({currentUser, isCartDropdownHidden}) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className='logo'/>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">
                 SHOP
-            </Link>
-            <Link className="option" to="/contacts">
+            </OptionLink>
+            <OptionLink to="/contacts">
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser
-                    ? <div className="option" onClick={() => auth.signOut()}> SIGN OUT </div>
-                    : <Link className="option" to="/signin"> SIGN IN </Link>
+                    ? <OptionLink as='div' className="option" onClick={() => auth.signOut()}> SIGN OUT </OptionLink>
+                    : <OptionLink to="/signin"> SIGN IN </OptionLink>
             }
             <CartIcon/>
-        </div>
+        </OptionsContainer>
         {isCartDropdownHidden ? null : <CartDropdown/>}
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
